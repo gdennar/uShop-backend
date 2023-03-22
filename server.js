f46e2782b5ee7d06cbdb4adb62279a3b209bab79
gold-dennar <gdennar@icitsolutions.com>
2023-03-22 16:48:14 +0100

    hot fix

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "build", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static("build"));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "build", "index.html"));
+// 	});
+// }
 
-// app.get("/", (req, res) => {
-// 	res.send("Welcome to uShop website");
-// });
+app.get("/", (req, res) => {
+	res.send("Welcome to uShop website");
+});
 
 //app.use(express.static("public"));
 
